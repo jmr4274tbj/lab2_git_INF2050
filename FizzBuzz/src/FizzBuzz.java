e7e97cb278070dc07fc74be581a53436bb15a6d1
jmr4274tbj <fivecores22@hotmail.com>
2021-02-05 14:56:05 -0500

    Ajout du programme FizzBuzz

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,24 @@
+
+public class FizzBuzz {
+
+	public static final int NB_ITERATION = 100; 
+	public static final int DIV_FIZZ = 3;
+	public static final int DIV_BUZZ = 5;
+	
+	public static void main(String[] args) {
+
+		for(int i = 1; i <= NB_ITERATION; i++) {
+			if ((i % DIV_FIZZ == 0) && (i % DIV_BUZZ == 0)) {
+				System.out.println("FizzBuzz");
+		    } else if (i % DIV_FIZZ == 0) {
+		    	System.out.println("Fizz");
+		    } else if (i % DIV_BUZZ == 0) {
+		    	System.out.println("Buzz");
+		    } else {
+		    	System.out.println(i);
+		    }
+		}		
+
+	}
+
+}
